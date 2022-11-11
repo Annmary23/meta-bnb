88ce23b7466d0ec68d172a88e819ee813229312b
kelani34 <kelanitaiwo34@gmail.com>
2022-11-11 14:25:34 +0100

    Finish Header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
-import Header from './components/header/header'
+import Header from './components/Header'
 
 function App() {
 
@@ -10,9 +10,9 @@ function App() {
     <>
       <Router>
         <Header>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-        </Routes>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+          </Routes>
         </Header>
       </Router>
     </>
